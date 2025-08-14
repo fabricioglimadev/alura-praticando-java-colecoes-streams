22dbf8d11d2eb92ba6982ddb71a78c26de0916da
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-08-13 21:39:42 -0300

    Filter, Reduce

--- src/Projeto02.java
+++ src/Projeto02.java
@@ -6,12 +6,21 @@ public class Projeto02 {
   public static void main(String[] args) {
 
     List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda");
-
     List<String> funcionariosLetraA = funcionarios.stream()
             .filter(f -> f.startsWith("A"))
             .collect(Collectors.toList());
     System.out.println(funcionarios);
     System.out.println(funcionariosLetraA);
+
+    List<Double> valorVendas = List.of(500.0, 1800.0, 6200.0);
+    List<Double> comissao = valorVendas.stream()
+            .map(v -> v * 0.05)
+            .collect(Collectors.toList());
+    System.out.println(comissao);
+
+    double totalVendas = valorVendas.stream()
+            .reduce(0.0, Double::sum);
+    System.out.println("Total vendas: " + totalVendas);
 
   }
 
